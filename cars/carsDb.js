9171868f0f66eb81ebdaba4121abc617aeb80cc1
Cristina Altreche <c.altreche@gmail.com>
2020-08-11 18:07:07 -0400

    Able to get by id

--- cars/carsDb.js
+++ cars/carsDb.js
@@ -2,8 +2,15 @@ const db = require("../seeds/dbConfig");
 
 module.exports = {
   get,
+  getById,
 };
 
 function get() {
   return db("cars");
+}
+
+function getById(id) {
+    return db('cars')
+    .where({id})
+    .first()
 }
--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -18,4 +18,15 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Cars.getById(id)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((data) => {
+      res.status(500).json({ error: "Something went wrong" });
+    });
+});
+
 module.exports = router;
