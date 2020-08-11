546c9bf6d2ff6f6f4844be82afccf34539c191a9
Cristina Altreche <c.altreche@gmail.com>
2020-08-11 18:01:05 -0400

    Able to pull data from new database

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -2,18 +2,20 @@ const express = require("express");
 const knex = require("knex");
 const Cars = require("../cars/carsDb");
 
-const db = knex({
-  client: "sqlite3",
-  connection: {
-    filename: "./data/car-dealer.db3",
-  },
-  useNullAsDefault: true,
-});
-
 const router = express.Router();
 
+// router.get("/", (req, res) => {
+//   res.status(200).json({ message: "Success from router!" });
+// });
+
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "Success!" });
+  Cars.get()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require("./server");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`Running on port ${port}`));
